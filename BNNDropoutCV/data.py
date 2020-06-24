a11f47145a1d18aeab1986086a3891056d7a5a16
duongna21 <38061659+duongna21@users.noreply.github.com>
2020-06-24 10:32:01 +0700

    Update data.py

--- BNNDropoutCV/data.py
+++ BNNDropoutCV/data.py
@@ -6,10 +6,10 @@ def get_svhn(batch_size):
     trsnform = transforms.Compose(
         [transforms.ToTensor(), transforms.Normalize((0.4378, 0.4439, 0.4729), (0.1980, 0.2011, 0.1971))])
     train_loader = torch.utils.data.DataLoader(
-        datasets.SVHN('./BNNDropoutCV/data', 'train', download=True,
+        datasets.SVHN('/home/ubuntu/data', 'train', download=True,
                       transform=trsnform), batch_size=batch_size, shuffle=True)
     test_loader = torch.utils.data.DataLoader(
-        datasets.SVHN('./BNNDropoutCV/data', 'test', download=True,
+        datasets.SVHN('/home/ubuntu/data', 'test', download=True,
                       transform=trsnform), batch_size=batch_size, shuffle=True)
     return train_loader, test_loader
 
@@ -19,12 +19,12 @@ def get_cifar(batch_size):
         [transforms.ToTensor(),
          transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))])
 
-    trainset = torchvision.datasets.CIFAR10(root='./BNNDropoutCV/data', train=True,
+    trainset = torchvision.datasets.CIFAR10(root='/home/ubuntu/data', train=True,
                                             download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                               shuffle=True, num_workers=2)
 
-    testset = torchvision.datasets.CIFAR10(root='./BNNDropoutCV/data', train=False,
+    testset = torchvision.datasets.CIFAR10(root='/home/ubuntu/data', train=False,
                                            download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                              shuffle=False, num_workers=2)
@@ -33,10 +33,10 @@ def get_cifar(batch_size):
 def get_mnist(batch_size):
     trsnform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
     train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('./BNNDropoutCV/data', train=True, download=True,
+        datasets.MNIST('/home/ubuntu/data', train=True, download=True,
                        transform=trsnform), batch_size=batch_size, shuffle=True)
     test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('./BNNDropoutCV/data', train=False, download=True,
+        datasets.MNIST('/home/ubuntu/data', train=False, download=True,
                        transform=trsnform), batch_size=batch_size, shuffle=True)
 
     return train_loader, test_loader
